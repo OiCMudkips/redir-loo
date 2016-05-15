dfe966841696f292ef5ea237d899f2b15df9e43c
OiCMudkips <victor_zhou1995@hotmail.ca>
2016-05-15 10:46:00 -0400

    spacing

--- redir-loo.js
+++ redir-loo.js
@@ -18,7 +18,7 @@ app.use(session({
 app.use(express.static('public'));
 
 // Configure body parser for POST requests
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json({ strict: true, type: 'application/json' }));
 
 // Route GET requests as appropriate
