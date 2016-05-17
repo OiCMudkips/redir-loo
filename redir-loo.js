85942e2956e99969351c79d2dc66eafad41aa5de
OiCMudkips <victor_zhou1995@hotmail.ca>
2016-05-17 02:34:04 -0400

    add SQL server; add ability for user to load their links

--- redir-loo.js
+++ redir-loo.js
@@ -3,9 +3,10 @@ var bodyParser = require('body-parser');
 var CASAuthentication = require('cas-authentication');
 var express = require('express');
 var session = require('express-session');
+var Client = require('mariasql');
 var whiskers = require('whiskers');
 
-// Config
+// App config
 var config = require('./config/config.json');
 var casService = new CASAuthentication(config.casOptions);
 var app = express();
@@ -16,6 +17,13 @@ app.use(session({
 }));
 app.engine('.whisker', whiskers.__express);
 app.set('views', __dirname + '/templates');
+
+// DB config
+var client = new Client(config.sqlOptions);
+var preparedUserLookup = client.prepare('SELECT * FROM USERS WHERE USERNAME=:username');
+var preparedUserInsert = client.prepare('INSERT INTO USERS (username) VALUES (:username)');
+var preparedUserDelete = client.prepare('DELETE FROM USERS WHERE USERNAME=:username LIMIT 1');
+var preparedLinkLookup = client.prepare('SELECT url, shortened FROM links WHERE id IN (SELECT b.linkid FROM users a, linkowners b WHERE a.username=:username AND a.userid=b.userid)');
 
 // Expose static resources
 app.use(express.static('public'));
@@ -29,16 +37,39 @@ app.get('/', function(request, response) {
 })
 
 app.get('/login', casService.bounce, function(request, response) {
-    response.redirect('/my-links');
+    client.query( preparedUserInsert({ username: request.session[casService.session_name] }),
+        function(error, rows) {
+            if (error && error.code === 1062) {
+                console.log('Existing user ' + request.session[casService.session_name] + ' logged in.');
+                response.redirect('/my-links');
+            }
+            else if (error) {
+                console.log(error);
+            }
+            else {
+                console.log('New user ' + request.session[casService.session_name] + ' logged in.');
+                response.redirect('/my-links');
+            }
+        }
+    );
 });
 
 app.get('/my-links', casService.bounce, function(request, response) {
-    response.render('user.whisker', { user: request.session[casService.session_name] });
+    client.query( preparedLinkLookup({ username: request.session[casService.session_name] }),
+        function(error, rows) {
+            if (error) {
+                console.log(error);
+            }
+            else {
+                response.render('user.whisker',
+                                { user: request.session[casService.session_name],
+                                  rows: rows });
+            }
+        }
+    );
 });
 
-app.get('/logout', casService.logout, function(request, response) {
-    response.send('Logged out!');
-});
+app.get('/logout', casService.logout);
 
 // Route POST requests as appropriate
 app.post('/create-link', casService.block, function(request, response) {
