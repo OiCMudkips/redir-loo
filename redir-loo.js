2d1be73d1a59009cee414d8083e455db6b49b914
OiCMudkips <victor_zhou1995@hotmail.ca>
2016-09-15 20:02:24 -0400

    use let & const

--- redir-loo.js
+++ redir-loo.js
@@ -1,17 +1,17 @@
 // Dependencies
-var bodyParser = require('body-parser');
-var CASAuthentication = require('cas-authentication');
-var express = require('express');
-var session = require('express-session');
-var Client = require('mariasql');
-var whiskers = require('whiskers');
-var shortid = require('shortid');
-var validUrl = require('valid-url');
+const bodyParser = require('body-parser');
+const CASAuthentication = require('cas-authentication');
+const express = require('express');
+const session = require('express-session');
+const Client = require('mariasql');
+const whiskers = require('whiskers');
+const shortid = require('shortid');
+const validUrl = require('valid-url');
 
 // App config
-var config = require('./config/config.json');
-var casService = new CASAuthentication(config.casOptions);
-var app = express();
+const config = require('./config/config.json');
+let casService = new CASAuthentication(config.casOptions);
+let app = express();
 app.use(session({
     secret: config.expressSessionSecret,
     resave: false,
@@ -21,13 +21,13 @@ app.engine('.html', whiskers.__express);
 app.set('views', __dirname + '/templates');
 
 // DB config
-var client = new Client(config.sqlOptions);
-var preparedUserLookup = client.prepare('SELECT * FROM USERS WHERE USERNAME=:username');
-var preparedUserInsert = client.prepare('INSERT INTO USERS (username) VALUES (:username)');
-var preparedUserDelete = client.prepare('DELETE FROM USERS WHERE USERNAME=:username LIMIT 1');
-var preparedUserLinksLookup = client.prepare('SELECT url, shortened FROM users a, links b WHERE a.username=:username AND a.userid=b.owner');
-var preparedUserLinksInsert = client.prepare('INSERT INTO links (url, shortened, owner) SELECT :link, :shortened, users.userid FROM users WHERE users.username=:username');
-var preparedLinkLookup = client.prepare('SELECT url FROM links WHERE shortened=:shortened');
+let client = new Client(config.sqlOptions);
+let preparedUserLookup = client.prepare('SELECT * FROM USERS WHERE USERNAME=:username');
+let preparedUserInsert = client.prepare('INSERT INTO USERS (username) VALUES (:username)');
+let preparedUserDelete = client.prepare('DELETE FROM USERS WHERE USERNAME=:username LIMIT 1');
+let preparedUserLinksLookup = client.prepare('SELECT url, shortened FROM users a, links b WHERE a.username=:username AND a.userid=b.owner');
+let preparedUserLinksInsert = client.prepare('INSERT INTO links (url, shortened, owner) SELECT :link, :shortened, users.userid FROM users WHERE users.username=:username');
+let preparedLinkLookup = client.prepare('SELECT url FROM links WHERE shortened=:shortened');
 
 // Create link constants
 ResponseCodes = {
@@ -40,7 +40,7 @@ ResponseCodes = {
 app.use(express.static('public'));
 
 // Configure body parser for POST requests
-var jsonParser = bodyParser.json({ strict: true, type: 'application/json' });
+let jsonParser = bodyParser.json({ strict: true, type: 'application/json' });
 
 // Route GET requests as appropriate
 app.get('/', function(request, response) {
@@ -102,7 +102,7 @@ app.get('/my-links', casService.bounce, function(request, response) {
 });
 
 app.get('/l/:id', casService.bounce, function(request, response) {
-    var shortenedId = request.params.id;
+    let shortenedId = request.params.id;
     if (!shortid.isValid(shortenedId)) {
         response.status(404).send();
         return;
@@ -134,7 +134,7 @@ app.post('/create-link', casService.block, jsonParser, function(request, respons
     // requirements for valid POST request:
     // url is a well-formatted HTTP or HTTPS link
 
-    var jsonResponse = {};
+    let jsonResponse = {};
 
     if (!validUrl.isWebUri(request.body.url)) {
         jsonResponse.code = ResponseCodes.BAD_URL;
@@ -143,7 +143,7 @@ app.post('/create-link', casService.block, jsonParser, function(request, respons
         return;
     }
 
-    var generatedId = shortid.generate();
+    let generatedId = shortid.generate();
     client.query(
         preparedUserLinksInsert({
             username: request.session[casService.session_name],
